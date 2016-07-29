2ff1d81fd36dd5337822a47b96fa6df8786d9135
duhlarik <DeeAnn.Uhlarik@gmail.com>
2016-07-29 16:44:34 -0400

    first commit

---
+++ Palindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String input = "";
+		boolean isPalindrome = true;
+
+		System.out.println("Please enter a word to check for palindromes: ");
+		input = scan.nextLine();
+
+		for (int i = 0; i < input.length() / 2; i++) {
+			if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
+				isPalindrome = false;
+				break;
+			}
+		}
+		if (isPalindrome) {
+			System.out.println("This is a Palindrome!");
+		} else {
+			System.out.println("This is not a Palindrome!");
+		}
+
+		scan.close();
+
+	}
+
+}
